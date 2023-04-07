a48b3ed1a0ed2cded6fdaa352f55d2893ebb7fb1
ecebos <ecebostan02@gmail.com>
2023-04-07 15:18:43 +0300

    ui-front

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+<button @click="handleClick" :class="['button', 'baseButton']">{{buttonText}}</button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    buttonText:{
+      type:String,
+      default:()=>"Label",
+    },
+    light:{
+      type:Boolean,
+      default:()=> true,
+    },
+  },
+  methods:{
+    handleClick(){
+      alert("Click me!");
+    }
+  }
+ 
+}
+</script>
+<style scoped>
+.button{
+  background-color: rgb(87, 3, 3);
+  color: white;
+  border: 1px solid gray;
+  cursor: pointer;
+}
+.baseButton{
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+  width: 100px;
+}
+</style>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HelloWorld :light="true" buttonText="Click"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  
+}
+</script>
+
